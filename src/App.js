96f7d99714c2167cbf9d3b0d1504d35bbd3a5d58
GlaudeEdenson <edeninform76@gmail.com>
2024-11-03 14:31:59 -0300

    Create hero-section, improve navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 // App.js
 import React from "react";
-import Layout from "./component/Layout"; 
+import Layout from "./component/Layout";
 import Service from "./pages/services";
 import About from "./pages/about";
 import Panier from "./pages/panier";
 import Product from "./pages/product";
+import Home from "./pages/home"
 import { Route, Routes } from "react-router-dom";
 import "./component/css/Home.css";
 
 function App() {
   return (
     <Layout>
+      {/* Les routes */}
       <Routes>
+      <Route path="/" element={<Home />} />
         <Route path="/services" element={<Service />} />
         <Route path="/about" element={<About />} />
         <Route path="/panier" element={<Panier />} />
---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../component/Layout";
+import Hero from "../component/hero/Hero";
+
+
+function home() {
+  return (
+    <Layout>
+      {/* Hero */}
+      <Hero />
+    </Layout>
+  );
+}
+
+export default home;
